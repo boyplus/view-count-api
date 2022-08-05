279b7fb9bba0480d29dee7743c1ac94ee5f751e0
Thanaphon Sombunkaeo <ict224bj@gmail.com>
2022-08-06 01:03:14 +0700

    add: test fetch data from db

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -6,7 +6,8 @@ import { Admin } from '../entity';
 export default class TestController extends Controller {
   @Get('/')
   @SuccessResponse('200', 'Tets route')
-  public async hello(): Promise<string> {
-    return 'test';
+  public async hello(): Promise<Admin[]> {
+    const admins = await AppDataSource.manager.find(Admin);
+    return admins;
   }
 }
